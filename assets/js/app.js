736df427f462b0ec88d698ae3a86dd35ec7076d5
flor condori <condorihuamanif@gmail.com>
2017-05-29 18:45:10 -0500

    te encuentra en el mapa

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,28 @@ function initMap() {
 	  zoom: 4,
 	  center: uluru
 	});
-	/*var marker = new google.maps.Marker({
-	  position: uluru,
-	  map: map
-	});*/
+
     // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        map.setCenter(pos);
+        var marker = new google.maps.Marker({
+		  position: pos,
+		  map: map,
+		  zoom : 18
+		});
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+
 
 }
