1533cd98afa732d85b5fd2053f12ac646623584f
flor condori <condorihuamanif@gmail.com>
2017-05-29 17:21:27 -0500

    trafica para deskpot

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-app
+function initMap() {
+	var uluru = {lat: -13.363, lng: 31.044};
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 4,
+	  center: uluru
+	});
+	/*var marker = new google.maps.Marker({
+	  position: uluru,
+	  map: map
+	});*/
+    // Try HTML5 geolocation.
+
+}
