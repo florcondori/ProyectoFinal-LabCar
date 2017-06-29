55b2190bc2ab139229e0696af8a32ead8315b59e
flor condori <condorihuamanif@gmail.com>
2017-06-29 04:07:08 -0500

    autocompletado y trazar ruta

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,69 @@
 function initMap() {
-	var uluru = {lat: -13.363, lng: 31.044};
+	var posInicial = {lat: -13.363, lng: 31.044};
 	var map = new google.maps.Map(document.getElementById('map'), {
 	  zoom: 4,
-	  center: uluru
+	  center: posInicial
 	});
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
+    // ubicarme.
+  var markadorUbicame;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-        map.setCenter(pos);
-        var marker = new google.maps.Marker({
-		  position: pos,
-		  map: map,
-		  zoom : 18
-		});
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
+      markadorUbicame = new google.maps.Marker({
+  		  position: pos,
+  		  map: map,  		 
+  		});
 
+      map.setCenter(pos);
+      map.setZoom(13);
 
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    alert("tu navegador no es compatible con Geolocation");
+  }
+
+  //Autocompletado
+  var partida = document.getElementById("punto-partida");
+  var llegada = document.getElementById("punto-llegada");
+
+  new google.maps.places.Autocomplete(partida);
+  new google.maps.places.Autocomplete(llegada);
+
+  //Trazar ruta
+ 
+
+  document.getElementById("trazar").addEventListener("click",function(e){
+    e.preventDefault();
+
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    var directionsService = new google.maps.DirectionsService();
+    
+    directionsService.route(
+      {
+        origin: partida.value,
+        destination: llegada.value,
+        travelMode: 'DRIVING' 
+      }, 
+      function(response, status) {
+        if (status == "OK") {
+          directionsDisplay.setMap(map); 
+          markadorUbicame.setMap(null);                 
+          directionsDisplay.setDirections(response);
+          console.log(response.routes[0].legs[0].distance.text);
+          var km = response.routes[0].legs[0].distance.text.replace(",",".").replace("km","")*1.75;
+
+          document.getElementById("km").classList.remove("invisible");
+          document.getElementById("km").innerHTML = "S/."+parseInt(km);
+        } else {
+            alert("No existen rutas entre ambos puntos");
+        }
+      }
+    );
+  });
 }
